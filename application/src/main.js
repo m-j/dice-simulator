c78d703735c9a94ecd3e1ab2a4d3f59135937f87
m-j <jamiolkowski.mateusz@gmail.com>
2013-10-12 10:13:57 +0100

    Setting up requirejs and html template

---
+++ application/src/main.js
@@ -0,0 +1,15 @@
+requirejs.config({
+    baseUrl : '/',
+    paths : {
+        'jquery' : 'lib/jquery/jquery'
+    },
+    shim : {
+        'jquery' : {
+            exports : '$'
+        }
+    }
+})
+
+require(['jquery'],function($){
+
+});
